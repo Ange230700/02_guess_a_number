c4365499e240d9943398c1b729383e221a3c7d99
Ange230700 <kouakouangeericstephane@gmail.com>
2024-10-26 19:10:41 +0200

    implemented a function checking if user won

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function askUserToInputNumber() {
-    const numberGivenByUser = parseInt(prompt("Input a number: "));
-    console.log(`The number given by the user is: ${numberGivenByUser}`);
-    return numberGivenByUser;
+function askPlayerToInputNumber() {
+    const numberGivenByPlayer = parseInt(prompt("Input a number: "));
+    console.log(`The number given by the player is: ${numberGivenByPlayer}`);
+    return numberGivenByPlayer;
 }
 
-const numberGivenByUser = askUserToInputNumber();
+const numberGivenByPlayer = askPlayerToInputNumber();
+
+const numberToGuess = 22;
+
+function checkIfPlayerGuessedTheNumber(numberGivenByPlayer) {
+    if (numberGivenByPlayer === numberToGuess) {
+        console.log(`Congratulations! The number is indeed ${numberToGuess}`);
+    } else {
+        if (numberGivenByPlayer > numberToGuess) {
+            console.log(`The number is too high. Try again!`);
+        } else {
+            console.log(`The number is too low. Try again!`);
+        }
+    }
+}
+
+checkIfPlayerGuessedTheNumber(numberGivenByPlayer);
